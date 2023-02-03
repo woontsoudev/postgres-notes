5bd194bc39ce172c4c31ce4e8f1b34aaea5e9fba
Woon Tsou <woontsoudev@gmail.com>
2023-02-03 11:37:31 -0600

    adding documentation

---
+++ JOIN_EXAMPLE.sql
@@ -0,0 +1,34 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50)
+);
+
+INSERT INTO users (username)
+VALUES
+	('monahan93'),
+  ('pferrer'),
+  ('si93onis'),
+  ('99stroman');
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id)
+);
+
+INSERT INTO photos (url, user_id)
+VALUES
+	('http://two.jpg', 1),
+  ('http://25.jpg', 1),
+  ('http://36.jpg', 1),
+  ('http://754.jpg', 2),
+  ('http://35.jpg', 3),
+  ('http://256.jpg', 4);
+
+SELECT * FROM photos;
+
+SELECT * FROM photos
+WHERE user_id = 4;
+
+SELECT url, username FROM photos
+JOIN users ON users.id = photos.user_id;
